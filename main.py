21df884c45a5f46ad63ecce579f3f19eb9dd7056
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-05-13 16:02:19 +0500

    Api endpoint added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from keras.models import load_model
+import pickle
+import numpy as np
 
 app = FastAPI()
 
@@ -9,6 +11,25 @@ model.compile()
 
 print(model.summary())
 @app.get("/")
-def read_root():
-    result = model.summary()
-    return {"result":result}
+
+
+
+def to_onehot(yy):
+    result = []
+    for num in yy:
+      result.append(round(num))
+    return result
+
+
+def classify():
+    mods = [b'8PSK', b'AM-DSB', b'BPSK', b'CPFSK', b'GFSK', b'PAM4', b'QAM16', b'QAM64', b'QPSK', b'WBFM']
+    file = open("/singleton_final.dat",'rb')
+    Xd = pickle.load(file)
+    new_data = np.array(Xd)
+    new_data = new_data[np.newaxis,...]
+    new_data.shape
+    result = model.predict(new_data)
+    final_result = to_onehot(result[0])
+    idx = final_result.index(1)
+    return {"modulation:type":mods[idx]}
+
